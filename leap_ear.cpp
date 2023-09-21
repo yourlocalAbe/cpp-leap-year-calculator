afd0ea824bf31a18721be5b17309c999d4d28be5
Abe <127510273+yourlocalAbe@users.noreply.github.com>
2023-09-22 06:04:40 +0700

    Update leap_year.cpp

--- leap_ear.cpp
+++ leap_ear.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-int main() {
-  int year;
 
-  std::cout << "Whats the year on ur mind?\n";
+int main() {
+  
+  int year;
+  
+  year >= 1000 && year <= 10000;
+
+  std::cout << "input year : \n";
   std::cin >> year;
-  if (year >= 1000 && year <= 9999 ) {
-    std::cout << "The year is " << year << "\n";
+
+  if (year > 999 || year <= 9999) {
+    std::cout << "the year of ur choice is\n" << year << "\n";
   } else {
-    std::cout << "Antum tolol\n";
+    std::cout << "something wrong definetly happening in ur head rn\n";
     return 0;
-  }
+  } 
 
   if (year % 4 == 0) {
     std::cout << "its a leap year\n";
-  } else if (year % 100 != 0 && year % 400 != 0) {
+  } else if (year % 100 == 0 || year % 400 != 0) {
     std::cout << "its not a leap year\n";
-  } 
+  } else if (year % 400 == 0) {
+    std::cout << "its a leap year\n";
+  
+}
 }
